cb5cedcef1a48048b661d5040da484a8574449d8
Anna Kryva <anna120100@gmail.com>
2021-05-10 20:21:49 +0300

    feat: send notifications on a new article creating

--- api/src/routes/articles.ts
+++ api/src/routes/articles.ts
@@ -1,6 +1,8 @@
 import cuid from 'cuid';
 import express from 'express';
 import Article from '../models/Article';
+import DeviceToken from '../models/DeviceToken';
+import { NotificationType, sendNotification } from '../utils/pushNotifications';
 
 const router = express.Router();
 
@@ -28,6 +30,25 @@ router.post('/', async (req, res) => {
   try {
     const id = cuid();
     const article = await Article.query().insert({ id, ...req.body });
+
+    const sendNotificationAsync = async () => {
+      const deviceTokens = await DeviceToken.query();
+
+      const tokens = deviceTokens.map(deviceToken => deviceToken.token);
+
+      const messageTitle = undefined; //default title for the message will be shown
+      const messageBody = article.title;
+
+      await sendNotification(
+        tokens,
+        NotificationType.TECH, //as an example
+        messageTitle,
+        messageBody,
+        { articleId: id },
+      );
+    };
+    // prevent request from waiting for the notification to be sent
+    sendNotificationAsync().catch(console.error);
 
     res.status(200).json({ article });
   } catch (error) {
