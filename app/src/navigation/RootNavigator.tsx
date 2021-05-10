6e1e85a2a815d3afa33eb8bd30a2e3f4f040be08
Anna Kryva <anna120100@gmail.com>
2021-05-10 03:51:37 +0300

    refactor: move notifications configurations from the RootNavigator to the separate hook useInitNotifications

--- app/src/navigation/RootNavigator.tsx
+++ app/src/navigation/RootNavigator.tsx
@@ -2,12 +2,11 @@ import {
   createStackNavigator,
   StackNavigationOptions,
 } from '@react-navigation/stack';
-import React, {useEffect, useMemo, useRef} from 'react';
+import React, {useMemo} from 'react';
 import Header from '../components/Header';
-import usePushNotifications from '../hooks/usePushNotifications';
+import useInitNotifications from '../hooks/useInitNotifications';
 import ArticleScreen from '../screens/Article';
 import MainScreen from '../screens/Main';
-import {initNotifications} from '../utils/pushNotifications';
 import {Article} from '../utils/types';
 import * as NavigationKeys from './NavigationKeys';
 
@@ -19,10 +18,7 @@ export type RootNavigatorParamList = {
 const Stack = createStackNavigator<RootNavigatorParamList>();
 
 const RootNavigator = () => {
-  const {syncNotifications} = usePushNotifications();
-
-  //To prevent calling syncNotifications function several times
-  const firstLoaded = useRef(true);
+  useInitNotifications();
 
   const commonScreenOptions = useMemo<StackNavigationOptions>(
     () => ({headerStyle: {backgroundColor: '#fff'}}),
@@ -33,19 +29,6 @@ const RootNavigator = () => {
     () => ({headerTitle: () => <Header />}),
     [],
   );
-
-  useEffect(() => {
-    if (!firstLoaded.current) {
-      return;
-    }
-
-    (async () => await syncNotifications())();
-    firstLoaded.current = false;
-  }, [syncNotifications]);
-
-  useEffect(() => {
-    initNotifications();
-  }, []);
 
   return (
     <Stack.Navigator
